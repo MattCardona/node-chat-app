450be6d414aeda8d5b80260b42338218eb4e9586
Matt <boblikesbbq@gmail.com>
2017-08-14 18:05:18 -0700

    Greet new user, and notify others

--- server/server.js
+++ server/server.js
@@ -18,6 +18,18 @@ io.on('connection', (socket) => {
     console.log('Client has disconnected');
   });
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the Chat Cha app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', function(message) {
     console.log('Created message', message);
     io.emit('newMessage', {
@@ -25,6 +37,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
 });
